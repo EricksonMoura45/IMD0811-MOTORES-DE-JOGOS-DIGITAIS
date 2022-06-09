cce948a220862c49c0153743270d998adfbbe0af
Erickson Moura <ericksonmoura45@gmail.com>
2022-06-08 23:35:03 -0300

    Remos Ok

---
+++ Assets/Scripts/RemoController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoController : MonoBehaviour
+{
+    public float posicaoDescanso = 0f;
+    public float posicaoPressionado = 45f;
+    public float forca = 10000f;
+    public float angRemo = 150f;
+
+    public string nome;
+    HingeJoint hinge;
+
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
+    void Update()
+    {
+
+        JointSpring spring = new JointSpring();
+        spring.spring = forca;
+        spring.damper = angRemo;
+                        
+        if(Input.GetAxis(nome) == 1)
+        {
+            spring.targetPosition = posicaoPressionado;
+        }
+        else
+        {
+            spring.targetPosition = posicaoDescanso;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+}
